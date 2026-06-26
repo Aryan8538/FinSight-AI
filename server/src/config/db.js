a0fd5458046c2cb2b597bc144c48500a8797c0c0
Aryan Agarwal <aryanagarwal245101@gmail.com>
2026-06-26 17:00:52 +0530

    Declare backend dependencies in root package.json for Vercel deployment, and reuse Mongoose connections

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { env } from "./env.js";
 
 export async function connectDatabase() {
+  if (mongoose.connection.readyState >= 1) return;
   mongoose.set("strictQuery", true);
   await mongoose.connect(env.mongoUri);
   console.log(`MongoDB connected: ${mongoose.connection.host}/${mongoose.connection.name}`);
